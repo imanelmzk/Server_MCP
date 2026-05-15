af0fa77f98876194fe321c8cf77a515dfef27616
imane.lmzk <imane.lmzk@gmail.com>
2026-05-15 17:21:10 +0100

    update

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { getAllUsers, createUser, getUserById, updateUser, deleteUser } from '../controllers/user.controller';
+
+const router = Router();
+
+router.get('/users', getAllUsers);
+router.post('/users', createUser);
+router.get('/users/:id', getUserById);
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
+
+export default router;
